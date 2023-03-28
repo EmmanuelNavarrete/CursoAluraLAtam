d5c7313aeb77eabf366e16a3ae2c4eacbcb8642e
EmmanuelNavarrete <s18030188@cdhidalgo.tecnm.mx>
2023-03-28 14:54:16 -0600

    alura

--- src/App.java
+++ src/App.java
@@ -13,7 +13,13 @@ public class App {
 		System.out.println("La edad de"+ nombre + "es igual a "+ edad);
 		System.out.println("El valor de pi es "+pi);
 		
-		//Se pueden realizar diferentes tipos de ejercicios
-		
+		//Se pueden realizar diferentes tipos de ejercicios o operaciones matematicas
+        //Como por ejemplo
+
+        double salario = 1000.394;
+        System.out.println("El salario es "+ salario);
+        System.out.println("El 15% del salario es "+ salario*0.15);
+        double mitadSalario = salario/2;
+        System.out.println("La mitad del salario es "+ mitadSalario);
 	}
 }
