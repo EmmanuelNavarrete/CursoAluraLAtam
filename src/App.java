4b532f1b19772c66aefaa49bf2f0c2bdb6924894
EmmanuelNavarrete <s18030188@cdhidalgo.tecnm.mx>
2023-03-28 14:52:01 -0600

    JavaAlura

---
+++ src/App.java
@@ -0,0 +1,19 @@
+
+public class App {
+
+	public static void main (String [] args) {
+		System.out.println("Hola Mundo!");
+		
+		//Como podemos observar tenemos diferentes tipos de datos
+		//Enteros
+		int edad =28;
+		String nombre = "Mauricio";
+		double pi = 3.1415;
+		
+		System.out.println("La edad de"+ nombre + "es igual a "+ edad);
+		System.out.println("El valor de pi es "+pi);
+		
+		//Se pueden realizar diferentes tipos de ejercicios
+		
+	}
+}
